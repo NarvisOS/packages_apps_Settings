a78b0553e77603ca4acb8fd22cd0ecf6aa409ffc
tronnayan <nayanarora55@gmail.com>
2020-08-12 13:38:12 +0000

    Added Build Maintainer name blob in about phone . Signed Off By : Nayan Arora <nayanarora55@gmail.com>

---
+++ src/com/android/settings/deviceinfo/firmwareversion/BuildMaintainerDetailPreferenceController.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2020 NarvisOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.deviceinfo.firmwareversion;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.SystemProperties;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+
+import androidx.preference.Preference;
+
+import com.android.settings.R;
+import com.android.settings.core.BasePreferenceController;
+
+public class BuildMaintainerDetailPreferenceController extends BasePreferenceController {
+
+    private static final Uri INTENT_URI_DATA = Uri.parse("https://paypal.me/tronnayan/");
+    private static final String TAG = "romDialogCtrl";
+    private static final String KEY_BUILD_MAINTAINER_PROP = "ro.narvis.build_maintainer";
+
+    private final PackageManager mPackageManager;
+
+    public BuildMaintainerDetailPreferenceController(Context context, String key) {
+        super(context, key);
+        mPackageManager = mContext.getPackageManager();
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return AVAILABLE;
+    }
+
+    @Override
+    public CharSequence getSummary() {
+        String buildMaintainer = SystemProperties.get(KEY_BUILD_MAINTAINER_PROP,
+                this.mContext.getString(R.string.device_info_default));
+        if (!buildMaintainer.isEmpty())
+            return buildMaintainer;
+        else
+            return mContext.getString(R.string.build_maintainer_default);
+    }
+
+    @Override
+    public boolean handlePreferenceTreeClick(Preference preference) {
+        if (!TextUtils.equals(preference.getKey(), getPreferenceKey())) {
+            return false;
+        }
+
+        final Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.setData(INTENT_URI_DATA);
+        if (mPackageManager.queryIntentActivities(intent, 0).isEmpty()) {
+            // Don't send out the intent to stop crash
+            Log.w(TAG, "queryIntentActivities() returns empty");
+            return true;
+        }
+
+        mContext.startActivity(intent);
+        return true;
+    }
+}
